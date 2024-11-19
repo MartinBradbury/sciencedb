6779a1da25d0164333ab7ef7749255c6e0cf3e77
Martin Bradbury <martinbradbury@btinternet.com>
2024-11-19 12:02:58 +0000

    created serializers for custom user, user registration and login along with validation methods.

---
+++ accounts/serializers.py
@@ -0,0 +1,40 @@
+from django.contrib.auth import authenticate
+from rest_framework import serializers
+from .models import *
+
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password1 = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'username', 'password1', 'password2', 'id')
+        extra_kwargs = {'password' : {"write_only": True}}
+
+        def validate(self, attrs):
+            password = attrs.get("password1", "")
+            if len(password) < 8:
+                raise serializers.ValidationError("password is too short")
+            if attrs['password1'] != attrs['password2']:
+                raise serializers.ValidationError("Passwords do not match")
+            return attrs
+
+        def create(self, validated_data):
+            password = validated_data.pop('password1')
+            validated_data.pop('password2')
+            return CustomUser.objects.create_user(password = password, **validated_data)
+
+class UserLoginSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Incorrect details")

08fbc2ef53dd4ad4e011a47fe98360995956e002
Martin Bradbury <martinbradbury@btinternet.com>
2024-11-19 13:12:08 +0000

    created the userregistration, userlogout and userlogin api view.

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from .serializers import *
@@ -6,24 +6,22 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
 from rest_framework import status
 
-class UserRegistrationAPIView(GenericAPIView):
-    permissions_classes = (AllowAny)
-    serializer_class = UserRegisterSerializer
 
+class UserRegistrationAPIView(GenericAPIView):
+    permission_classes = (AllowAny, )
+    serializer_class = UserRegisterSerializer
     def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data = request.data)
-        serializer.is_valid(raise_exception =True)
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
         user = serializer.save()
         token = RefreshToken.for_user(user)
         data = serializer.data
-        data['token'] = {"refresh": str(token),
-                         "access" : str(token.access_token)}
+        data['token'] = {"refresh": str(token), "access": str(token.access_token)}
         return Response(data, status=status.HTTP_201_CREATED)
 
 class UserLoginAPIView(GenericAPIView):
-    permissions_classes = (AllowAny)
+    permission_classes = (AllowAny, )
     serializer_class = UserLoginSerializer
-
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data = request.data)
         serializer.is_valid(raise_exception =True)

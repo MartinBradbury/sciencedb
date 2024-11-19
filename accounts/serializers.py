546c6e8cc22571e7b767c47968ec98aafb2bd6ae
Martin Bradbury <martinbradbury@btinternet.com>
2024-11-19 13:26:55 +0000

    added url pathing

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -30,11 +30,12 @@ class UserRegisterSerializer(serializers.ModelSerializer):
             validated_data.pop('password2')
             return CustomUser.objects.create_user(password = password, **validated_data)
 
-class UserLoginSerializer(serializers.ModelSerializer):
+class UserLoginSerializer(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True)
+
     def validate(self, data):
         user = authenticate(**data)
         if user and user.is_active:
             return user
-        raise serializers.ValidationError("Incorrect details")
+        raise serializers.ValidationError("incorrect credentials")
